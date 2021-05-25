40fa55ce1db3c2b1673163c59e19cede171477f8
NgatiaJeffers <jeffersongakuya@gmail.com>
2021-05-25 10:56:22 +0300

    Create admin and add content

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView #ListView returns an object object_list
+from .models import Post
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Post
     template_name = 'home.html'
+    context_object_name = 'all_post_list'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
